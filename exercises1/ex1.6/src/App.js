18eec35ff7504c3f3efd6906ea13f563f178543b
Jorge Filipe <jfms777@gmail.com>
2021-04-26 22:24:22 -0300

    ex1.6 done

--- exercises1/ex1.6/src/App.js
+++ exercises1/ex1.6/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Buttons = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={props.handleClick}>{props.text}</button>
+    </>
+  )
 }
 
-export default App;
+
+const Display = () => {
+  return (
+    <>
+      <h1>give feedback</h1>
+    </>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.good === 0 && props.bad === 0 && props.neutral === 0) {
+    return (
+      <div>
+        <h1>{props.title}</h1>
+        no feedback given
+      </div>
+    )
+  }
+  return (
+    <div>
+      <Statistic text="good" value={props.good} />
+      <Statistic text="neutral" value={props.neutral} />
+      <Statistic text="bad" value={props.bad} />
+    // ...
+    </div>
+  )
+}
+
+const Statistic = (props) => {
+  return (
+    <div>
+      <table>
+        <tr>
+          <td>{props.text}</td>
+          <td>{props.value}</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Display />
+      <Buttons handleClick={() => setGood(good + 1)} text="good" />
+      <Buttons handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Buttons handleClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} title="statistics" />
+    </div>
+  )
+}
+
+export default App
