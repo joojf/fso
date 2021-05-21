e388bd9825bfce76b238374057979bc57b645bf7
Jorge Filipe <jfms777@gmail.com>
2021-05-21 18:17:25 -0300

    restarting

--- exercises2/countries/src/App.js
+++ exercises2/countries/src/App.js
@@ -1,59 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const DisplayFilter = (props) => {
-  if (props.countries.length > 10) {
-    return <p>too many matches, specify another filter.</p>;
-  } else {
-    return (
-      <ul>
-        {props.countries.map((country) => (
-          <p key={country.numericCode}>country={country}</p>
-        ))}
-      </ul>
-    );
-  }
-}
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState([])
-  const [countrySearch, setCountrySearch] = useState('')
-
-  useEffect(() => {
-    axios
-      .get('https://restcountries.eu/rest/v2/all').then(response => {
-        setCountries(response.data)
-      })
-  }, [])
-
-  const countriesFilter = (props) => {
-
-    const match = props.countries.filter((country) =>
-      country.name.toLowerCase().includes(props.countriesFilter.toLowerCase()),
-    )
-
-    if (props.countriesFilter.length === 0) {
-      return null
-    }
-
-    if (match.length > 1) {
-      return <DisplayFilter countries={match} />;
-    } else if (match.length > 0) {
-      const matchCountry = match[0];
-      return <DisplayFilter country={matchCountry} />;
-    } else {
-      return <p>No matches</p>;
-    }
-  }
-
   return (
-    <div>
-      <h1>Hello world!</h1>
-      country to search:<input value={countrySearch} onChange={(event) => setCountrySearch(event.target.value)} />
-      <DisplayFilter countriesFilter={countrySearch} countries={countries} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
