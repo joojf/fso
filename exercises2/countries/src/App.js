66d4f5d201712ba71c986d7be0809de4d51fd006
Jorge Filipe <jfms777@gmail.com>
2021-05-25 21:08:56 -0300

    step1

--- exercises2/countries/src/App.js
+++ exercises2/countries/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Countries = ({ filter, countries }) => {
+  const countriesToShow = countries.filter(country =>
+    country.name.toLowerCase().includes(filter.toLowerCase()))
+
+  if (countriesToShow.length <= 10) {
+    return (
+      countriesToShow.map(country =>
+        <p key={country.numericCode}>{country.name}</p>
+      )
+    )
+  }
+  else {
+    return (
+      <div>
+        too many matches, specify another filter
+      </div>
+    )
+  }
+
 }
 
-export default App;
+const Filter = ({ value, onChange }) => {
+  return (
+    <div>
+      find countries <input value={value} onChange={onChange} />
+    </div>
+  )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all').then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+
+  return (
+    <div>
+      <Filter value={filter} onChange={(event) => setFilter(event.target.value)} />
+      <Countries filter={filter} countries={countries} />
+    </div>
+  )
+}
+
+export default App
