f5a093cb004229bc637ecf0f10e5b7975c4e6ccb
Jorge Filipe <jfms777@gmail.com>
2021-07-15 09:08:32 -0300

    3.3: adding forgotten error message

--- exercises3/3/index.js
+++ exercises3/3/index.js
@@ -43,7 +43,11 @@ app.get('/info', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find((person) => person.id === id)
-    res.json(person)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
 })
 
 const PORT = 3001
