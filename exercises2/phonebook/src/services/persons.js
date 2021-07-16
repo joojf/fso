605b3b2cb8a71eea0c07f632bcf7c06c48a17e6a
Jorge Filipe <jfms777@gmail.com>
2021-07-16 10:25:25 -0300

    3.9 phonebook backend step9

--- exercises2/phonebook/src/services/persons.js
+++ exercises2/phonebook/src/services/persons.js
@@ -1,27 +1,28 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+    const request = axios.get(baseUrl)
+    return request.then((response) => response.data)
 }
 
 const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
+    const request = axios.post(baseUrl, newObject)
+    return request.then((response) => response.data)
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then((response) => response.data)
 }
 
 const remove = (personObject) => {
-  if(window.confirm(`Delete ${personObject.name}?`))  {
-    return axios.delete(`${baseUrl}/${personObject.id}`)
-    .then(response => response.data)
-  }
+    if (window.confirm(`Delete ${personObject.name}?`)) {
+        return axios
+            .delete(`${baseUrl}/${personObject.id}`)
+            .then((response) => response.data)
+    }
 }
 
 export default { getAll, create, update, remove }
--- exercises3/3/index.js
+++ exercises3/3/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 let persons = [
     {
@@ -26,6 +27,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (req, res) => {
     return JSON.stringify(req.body)
