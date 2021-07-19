408b53bac69d265535a6a96dbc49ee963fd6c97f
Jorge Filipe <jfms777@gmail.com>
2021-07-19 15:27:02 -0300

    3.12: Command-line database

---
+++ exercises3/3/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Usage: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.izsb3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+    const person = new Person({
+        name,
+        number,
+    })
+
+    person.save().then(() => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then((people) => {
+        people.forEach((person) => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
