fc28d68c15b3f166f5c80ba9ebb7c456e270f0b7
Jorge Filipe <jfms777@gmail.com>
2021-07-20 14:07:28 -0300

    3.18*: Phonebook database step6

--- exercises3/3/index.js
+++ exercises3/3/index.js
@@ -6,29 +6,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const { response } = require('express')
 
-let persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456',
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-5323523',
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-234345',
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendieck',
-        number: '39-23-6423122',
-    },
-]
-
 app.use(express.json())
 app.use(cors())
 
@@ -66,9 +43,9 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    res.send(
-        `Phonebook has info for ${persons.length} people <br/> <br/>${Date()}`
-    )
+    Person.countDocuments().then((count) => {
+        res.send(`Phonebook has info for ${count} people <br/> <br/>${Date()}`)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -87,13 +64,13 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
     //delete the person
-    Person.findByIdAndRemove(req.params.id)
-        .then((result) =>
-            response.status(204).send(`Person ${req.params.id} deleted`)
-        )
-        .catch((err) => {
-            next(err)
-        })
+    Person.findByIdAndRemove(req.params.id).then((person) => {
+        if (person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
+    })
 })
 
 app.post('/api/persons', (req, res) => {
