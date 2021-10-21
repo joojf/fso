5c7202558ca01e28f4f6ca7a9968228492b7b154
Jorge Filipe <jfms777@gmail.com>
2021-10-21 14:29:18 -0300

    8.5: Books by genre

--- exercises8/backend/index.js
+++ exercises8/backend/index.js
@@ -95,7 +95,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -105,10 +105,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (args.author) {
+        return books.filter(book => book.author === args.author)
+      } else if (args.genre) {
+        return books.filter(book => book.genres.includes(args.genre))
+      } else {
         return books
       }
-      return books.filter(book => book.author === args.author)
     },
     allAuthors: () => {
       return authors.map(a => {
